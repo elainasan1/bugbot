8dc8f62dbf3aaa45bcaedef5907b5832d4bbff89
elainasan1 <108674493+elainasan1@users.noreply.github.com>
2023-01-17 18:37:41 +0700

    Add files via upload

---
+++ baseik/command/rpgmenu/rpgmenu.js
@@ -0,0 +1,18 @@
+const rpgmenu =  ` 
+╔─═─═⊱ 「 *RFG MENU* 」 ─═─═⬣
+│┏⊱
+║⿻ leaderboard
+║⿻ inventori
+║⿻ mining
+║⿻ beli
+║⿻ jual
+║⿻ heal
+║⿻ berburu
+│┗⊱
+┗━━━━━━━━━━━━━━━━━━⬣
+┏━━⬣  Thanks To  友
+┃ 🔥 Hw Mods Wa
+┃ 🔥 DaniMakerDaniMaker
+┃ 🔥 Gilzzxy
+┗━━⬣  ⿻ Introvert ⿻`
+exports.rpgmenu = rpgmenu
